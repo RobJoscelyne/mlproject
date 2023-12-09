40930035a41296d8d26c2ec128dc903615e5a73c
RJ <robjoscelyne@yahoo.co.uk>
2023-12-09 15:09:49 +0000

    Updated name of app to application

--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
 
-HYPEN_E_DOT='-e .'
+HYPHEN_E_DOT = '-e .'
+
 def get_requirements(file_path:str)->List[str]:
     '''
-    this function will return the list of requirements
+    This function will return list of requirements
+    from requirements.txt file
     '''
-    requirements=[]
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+    # Initializing blank requirements list
+    requirements = []
 
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-    
+    # Opening the file
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+        
+        # Remove hyphen_e_dot if present in requirements
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
     return requirements
 
 setup(
--- test.py
+++
@@ -1,8 +0,0 @@
-# Test Script
-from src.utils import load_object
-
-preprocessor_path = r'C:\Users\robjo\mlproject\artifacts\proprocessor.pkl'
-preprocessor = load_object(preprocessor_path)
-print(preprocessor)
-
-
